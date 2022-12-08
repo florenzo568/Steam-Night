0e9888d7e69301a514947468e31b9006f9804885
florenzo568 <florenzo568@gmail.com>
2022-12-08 10:48:52 -0800

    fixed damage glitch

--- Steam Nights/Assets/Scripts/P1Attacking.cs
+++ Steam Nights/Assets/Scripts/P1Attacking.cs
@@ -7,6 +7,17 @@ public class P1Attacking : MonoBehaviour
     [SerializeField] P1Move P1;
     [SerializeField] P2Health P2H;
     [SerializeField] P1Blocking P1B;
+    [SerializeField] P1Light P1L;
+    [SerializeField] P1DownLight P1DL;
+    [SerializeField] P1JumpLight P1JL;
+    [SerializeField] P1Medium P1M;
+    [SerializeField] P1DownMedium P1DM;
+    [SerializeField] P1JumpMedium P1JM;
+    [SerializeField] P1Heavy P1H;
+    [SerializeField] P1DownHeavy P1DH;
+    [SerializeField] P1JumpHeavy P1JH;
+    [SerializeField] P1Special1 P1S1;
+    [SerializeField] P1Special4 P1S4;
     void Start()
     {
         
@@ -18,7 +29,7 @@ public class P1Attacking : MonoBehaviour
         if (Input.GetButton("Block") && Input.GetButtonDown("Fire1") && P1.IsGrounded() && P1B.Blocking == true && P1.crouch)
         {
             Debug.Log("Special Innate");
-            StartCoroutine(P1.GetComponent<P1Special1>().Special());
+            StartCoroutine(P1S1.Special());
             P1B.Blocking = false;
             P1.DragP = true;
         }
@@ -35,43 +46,43 @@ public class P1Attacking : MonoBehaviour
         if (Input.GetButton("Block") && Input.GetButtonDown("Fire3") && P1.IsGrounded() && P1B.Blocking == true && !P1.crouch && P1.canMove)
         {
             Debug.Log("Special 4");
-            StartCoroutine(P1.GetComponent<P1Special4>().Special());
+            StartCoroutine(P1S4.Special());
         }
         if (Input.GetButtonDown("Fire1") && P1.IsGrounded() && !Input.GetKey("s") && P1.canMove && P1B.Blocking == false)
         {
-            StartCoroutine(P1.GetComponent<P1Light>().Light());
+            StartCoroutine(P1L.Light());
         }
         if (Input.GetButtonDown("Fire1") && P1.IsGrounded() && Input.GetKey("s") && P1B.Blocking == false)
         {
-            StartCoroutine(P1.GetComponent<P1DownLight>().Light());
+            StartCoroutine(P1DL.Light());
         }
         if (Input.GetButtonDown("Fire1") && P1.IsGrounded() == false && P1.canMove)
         {
-            StartCoroutine(P1.GetComponent<P1JumpLight>().Light());
+            StartCoroutine(P1JL.Light());
         }
         if (Input.GetButtonDown("Fire2") && P1.IsGrounded() && !Input.GetKey("s") && P1.canMove && P1B.Blocking == false)
         {
-            StartCoroutine(P1.GetComponent<P1Medium>().Medium());
+            StartCoroutine(P1M.Medium());
         }
         if (Input.GetButtonDown("Fire2") && P1.IsGrounded() && Input.GetKey("s"))
         {
-            StartCoroutine(P1.GetComponent<P1DownMedium>().Medium());
+            StartCoroutine(P1DM.Medium());
         }
         if (Input.GetButtonDown("Fire2") && P1.IsGrounded() == false && P1.canMove)
         {
-            StartCoroutine(P1.GetComponent<P1JumpMedium>().Medium());
+            StartCoroutine(P1JM.Medium());
         }
         if (Input.GetButtonDown("Fire3") && P1.IsGrounded() && !Input.GetKey("s") && P1.canMove && P1B.Blocking == false)
         {
-            StartCoroutine(P1.GetComponent<P1Heavy>().Heavy());
+            StartCoroutine(P1H.Heavy());
         }
         if (Input.GetButtonDown("Fire3") && P1.IsGrounded() && Input.GetKey("s"))
         {
-            StartCoroutine(P1.GetComponent<P1DownHeavy>().Heavy());
+            StartCoroutine(P1DH.Heavy());
         }
         if (Input.GetButtonDown("Fire3") && P1.IsGrounded() == false && P1.canMove)
         {
-            StartCoroutine(P1.GetComponent<P1JumpHeavy>().Heavy());
+            StartCoroutine(P1JH.Heavy());
         }
     }
 }
