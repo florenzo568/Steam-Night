f905864bf0b32c48e89ac8720bd876297c06213b
florenzo568 <florenzo568@gmail.com>
2022-12-13 11:35:55 -0800

    Working scene reload and round counters

--- Steam Nights/Assets/Scripts/Misc/Rounds.cs
+++ Steam Nights/Assets/Scripts/Misc/Rounds.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Rounds : MonoBehaviour
 {
     public static int P1W = 0;
     public static int P2W = 0;
+    [SerializeField] TextMeshProUGUI P1Wins;
+    [SerializeField] TextMeshProUGUI P2Wins;
     void Start()
     {
         
@@ -14,6 +18,7 @@ public class Rounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        P1Wins.text = P1W.ToString();
+        P2Wins.text = P2W.ToString();
     }
 }
